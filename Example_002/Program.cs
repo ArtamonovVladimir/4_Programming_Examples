4dc938af4be333cdac8dbe290c5a0aecb76b6687
Vladimir_Artamonov <tomodachiva@gmail.com>
2022-06-23 13:49:41 +0300

    Example_002 with ToLower was added

---
+++ Example_002/Program.cs
@@ -0,0 +1,12 @@
+﻿// IF и ELSE команды
+Console.WriteLine("Введите свое имя: ");
+string userName = Console.ReadLine();
+if (userName.ToLower() == "катя")
+{
+    Console.WriteLine("Ура, это же Катя!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(userName);
+}
